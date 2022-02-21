b80bb3db65c9250c0973ae8ab06e0e7bbd939336
Chin-Ho-Yao <jack.yao@logicsolutions.com>
2022-02-21 13:16:41 +0800

    first commit

---
+++ src/main/java/com/yao/springhibernateforbeginners/SpringHibernateforBeginnersApplication.java
@@ -0,0 +1,13 @@
+package com.yao.springhibernateforbeginners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernateforBeginnersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringHibernateforBeginnersApplication.class, args);
+    }
+
+}
